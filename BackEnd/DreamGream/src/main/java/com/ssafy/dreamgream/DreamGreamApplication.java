8887dc6069d2505ccfcce3e2062ca9ea6ee87151
jlal1226 <jlal1226@naver.com>
2023-07-21 09:11:13 +0900

    spring project 추가

---
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/DreamGreamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dreamgream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamGreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamGreamApplication.class, args);
+    }
+
+}
---
+++ BackEnd/DreamGream/src/test/java/com/ssafy/dreamgream/DreamGreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.dreamgream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamGreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
