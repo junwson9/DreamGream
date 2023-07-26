0615d68f58f65ae164f365fb3e5b397acf691c45
HBumzz <choi12920@naver.com>
2023-07-26 11:02:33 +0900

    FEAT: 로그인 관련 기능 구현

---
+++ FrontEnd/dream-gream/src/pages/signUp_nicknamePage.jsx
@@ -0,0 +1,14 @@
+// 로그인하고
+import React from 'react';
+
+function signUp_nickname() {
+  return (
+    <div>
+      <p>환영합니다</p>
+      <p>~~~</p>
+      <input />
+      <button>초기화</button>
+    </div>
+  );
+}
+export default signUp_nickname;
