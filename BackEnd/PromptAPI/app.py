0110c278157dd870ba7025ae34cdcb909a043c1e
kimjunhyun <idkim8671@gmail.com>
2023-07-26 09:01:46 +0900

    FIX: Prompt 응답 정확도 향상 및 주석 추가 테스팅을 통한 Prompt 응답 고도화 [꼬리말] 초고도화 요망

--- BackEnd/PromptAPI/app.py
+++ BackEnd/PromptAPI/app.py
@@ -7,13 +7,12 @@ import asyncio
 from langchain import PromptTemplate
 import json
 
-
 # OpenAI API 액세스 키 설정
 load_dotenv()
 openai.api_key = os.getenv("API_KEY")
 app = FastAPI()
 
-# 요청 모델 정의
+#요청 형식 정의
 class PromptRequest(BaseModel):
     user_id: str
     prompt_id: str
@@ -22,32 +21,24 @@ class PromptRequest(BaseModel):
     gender: str
     category: str
 
-class PromptTemplateJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, PromptTemplate):
-            return obj.__dict__
-        return super().default(obj)
-
+#프롬프트 비동기 요청 함수
 async def chat_gpt3(prompt):
     response = await asyncio.to_thread(openai.Completion.create,
-                                       engine='text-davinci-003',
-                                       prompt=prompt,
-                                       max_tokens=50,
-                                       temperature=0.7,
-                                       n=3,
-                                       stop=None,
-                                       timeout=10)
+                                       engine='text-davinci-003', #gpt 엔진 종류
+                                       prompt=prompt, #prompt 변수 전달
+                                       max_tokens=50, #응답 길이 설정
+                                       temperature=0.7, #낮을수록 일관성 상승 / 높을수록 다양성 상승
+                                       n=1, #답변 받을 개수
+                                       stop=None, #일정 토큰량 넘어가면 함수 호출 스탑
+                                       timeout=10 #응답 최대 대기 시간
+                                    )
+    #응답에서 필요한 부분만 추출
     if 'choices' in response and len(response['choices']) > 0:
         choices = response['choices']
         return {'responses': [choice['text'].strip() for choice in choices]}
     return None
 
-#서버 잘 도는지 테스트
-@app.get("/")
-async def main():
-    return "Hi"
-
-#프롬프트 테스트
+#프롬프트 생성 요청
 @app.post("/prompt")
 async def process_prompt(request: PromptRequest):
     user_id = request.user_id
@@ -57,14 +48,13 @@ async def process_prompt(request: PromptRequest):
     gender = request.gender
     category = request.category
     
-    print(age)
-
     #전처리 부분
     if gender == "F":
         gender = "여성"
     else:
         gender = "남성"
     
+    #LangChain PromptTemplate 사용
     prompt = PromptTemplate(
         input_variables=["age", "gender", "content", "category"],
         template=
@@ -73,16 +63,10 @@ async def process_prompt(request: PromptRequest):
                 + "문장의 시작은 'a photo of'로 시작해."
     )
     
-    #print(content)
-
+    #Template에 값 대입
     prompt = prompt.format(age=age, gender=gender, content=content, category=category)
-    print(prompt)
-    #prompt_json = json.dumps(prompt, cls=PromptTemplateJSONEncoder)
-    #print(prompt_json)
 
-    #response = await chat_gpt3(prompt_json)
+    #API 비동기 요청
     response = await chat_gpt3(prompt)
-    print(response)
-    #request to generate_image_url
-    
+
     return {"user_id": user_id, "prompt_id": prompt_id, "response": response}
