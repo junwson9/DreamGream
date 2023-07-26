2502fd7914b18d13be85a4804d748fd169fd1579
송준우 <boaz42@khu.ac.kr>
2023-07-26 16:53:37 +0900

    FEAT: 그림 생성 화면 구현 MVP인 그림생성화면 초기작업 구현 백엔드서버와 연동하여 테스트를 해야함

--- FrontEnd/dream-gream/src/App.js
+++ FrontEnd/dream-gream/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import {Routes, Route} from 'react-router-dom'
 import PostSubject from './pages/Posting/postSubject';
+import PostViewImage from './pages/Posting/postViewImage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/post" element={ <PostSubject/> }/>
+      <Route path='/post' element={ <PostSubject/> }/>
+      <Route path='/image' element={ <PostViewImage/> }/>
     </Routes>
     
   );
