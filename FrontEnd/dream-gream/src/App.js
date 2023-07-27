4a6ce0a4ef68c098960d55a98365711b3e1785ed
송준우 <boaz42@khu.ac.kr>
2023-07-27 16:02:17 +0900

    FIX: 머지 오류 해결

--- FrontEnd/dream-gream/src/App.js
+++ FrontEnd/dream-gream/src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import React from 'react';
+
 import {Routes, Route} from 'react-router-dom'
 import PostSubject from './pages/Posting/postSubject';
 import PostViewImage from './pages/Posting/postViewImage';
+import LogIn from './pages/Login/Logintest';
+
 
 function App() {
   return (
     <Routes>
       <Route path='/post' element={ <PostSubject/> }/>
       <Route path='/image' element={ <PostViewImage/> }/>
+      <Route path="/login" element={<LogIn />} />
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
+      {/* <Route path="/" element={}/> */}
     </Routes>
-    
   );
 }
 
