6444597b044f2e7e5c98d23330e399b46e599586
Jisoo <spritaa@gmail.com>
2023-07-30 04:45:14 +0900

    RENAME: auth, member 패키지 정리
    jwt 관련 이슈 및 ROLE_GUEST->USER와 관련 이슈 auth 패키지로 이동

---
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/auth/service/AuthService.java
@@ -0,0 +1,16 @@
+package com.ssafy.dreamgream.global.auth.service;
+
+import com.ssafy.dreamgream.domain.member.enums.Gender;
+import com.ssafy.dreamgream.global.auth.dto.request.TokenRequestDto;
+import com.ssafy.dreamgream.global.jwt.TokenDto;
+import java.time.Year;
+import org.springframework.security.core.Authentication;
+
+public interface AuthService {
+
+	public TokenDto reissue(TokenRequestDto tokenRequestDto);
+
+	TokenDto updateRoleToUser(Long id, Gender gender, Year birthyear);
+
+	void saveRefreshTokenRedis(Authentication authentication, TokenDto tokenDto);
+}
