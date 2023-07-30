610e9480deea860c84f772fad9999113b93b98bb
Jisoo <spritaa@gmail.com>
2023-07-30 20:32:23 +0900

    REFACTOR: JWT 재발급 코드 리팩토링
    token을 request header로 받도록 수정, 중복 코드 제거

--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/jwt/JwtAuthenticationFilter.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,10 @@
 package com.ssafy.dreamgream.global.jwt;
 
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,14 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 @Slf4j
@@ -32,29 +30,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		FilterChain filterChain) throws ServletException, IOException {
 
 		// Request Header에서 JWT 추출
-		String token = resolveToken((HttpServletRequest) request);
-		log.debug("accessToken: {}", token);
+		String accessToken = jwtTokenProvider.resolveAccessToken((HttpServletRequest) request);
+		log.debug("accessToken: {}", accessToken);
 
 		// validateToken으로 유효성 검사
-		if (token != null && jwtTokenProvider.validateToken(token)) {
+		if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
 			// Redis에 해당 accessToken logout 여부 확인
-			String isLogout = (String) redisTemplate.opsForValue().get(token);
-			// refreshToken이 없는 경우, Authentication 객체를 가져와 SecurityContext에 저장
+			String isLogout = (String) redisTemplate.opsForValue().get(accessToken);
+			// 로그아웃 되어있지 않으면 Authentication 객체를 가져와 SecurityContext에 저장
 			if (ObjectUtils.isEmpty(isLogout)) {
-				Authentication authentication = jwtTokenProvider.getAuthentication(token);
+				Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			}
 		}
 
 		// 다음 필터로 request, response 전달
 		filterChain.doFilter(request, response);
-	}
-	private String resolveToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TYPE)) {
-			return bearerToken.substring(7);
-		}
-		return null;
 	}
 
 }
