1a9eb51196f5ce93d71224e9ef7c69a4e2d3e313
HBumzz <choi12920@gmail.com>
2023-07-31 07:35:28 +0900

    FEAT: 회원기능 구현

--- FrontEnd/dream-gream/src/App.js
+++ FrontEnd/dream-gream/src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import React from 'react';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import PostSubject from './pages/Posting/postSubject';
 import PostViewImage from './pages/Posting/postViewImage';
+import LogIn from './pages/members/Logintest';
+import OAuth2RedirectHandler from './pages/members/OAuth2RedirectHandler';
+import SiginupGenderBirth from './pages/members/SiguUp';
+import ProfileEdit from './pages/members/ProfileEdit';
 
 function App() {
   return (
     <Routes>
-      <Route path='/post' element={ <PostSubject/> }/>
-      <Route path='/image' element={ <PostViewImage/> }/>
+      <Route path="/post" element={<PostSubject />} />
+      <Route path="/image" element={<PostViewImage />} />
+      <Route path="/LogIn" element={<LogIn />} />
+      <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+      <Route path="/SiginupGenderBirth" element={<SiginupGenderBirth />} />
+      <Route path="/ProfileEdit" element={<ProfileEdit />} />
     </Routes>
-    
   );
 }
 
