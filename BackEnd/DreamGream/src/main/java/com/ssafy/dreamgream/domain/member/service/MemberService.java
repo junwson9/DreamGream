7028668aaa6dba740edf3374b50b03919c4321b9
Jisoo <spritaa@gmail.com>
2023-07-31 02:30:13 +0900

    FEAT: 회원 정보 조회, 회원 정보 수정 기능 추가
    회원 정보 조회, 회원 정보 수정 기능 추가

--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/domain/member/service/MemberService.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/domain/member/service/MemberService.java
@@ -1,9 +1,14 @@
 package com.ssafy.dreamgream.domain.member.service;
 
 import com.ssafy.dreamgream.domain.member.entity.Member;
+import com.ssafy.dreamgream.domain.member.enums.Gender;
+import java.util.List;
+import org.springframework.security.core.AuthenticationException;
 
 public interface MemberService {
 
-    public Member getCurrentMember();
+    Member getCurrentMember() throws AuthenticationException;
+
+	Member updateInfo(String nickname, Gender gender, Integer birthyear);
 
 }
--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/domain/member/service/MemberServiceImpl.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/domain/member/service/MemberServiceImpl.java
@@ -1,14 +1,18 @@
 package com.ssafy.dreamgream.domain.member.service;
 
 import com.ssafy.dreamgream.domain.member.entity.Member;
+import com.ssafy.dreamgream.domain.member.enums.Gender;
 import com.ssafy.dreamgream.domain.member.repository.MemberRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -18,7 +22,7 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
 
     @Override
-    public Member getCurrentMember() {
+    public Member getCurrentMember() throws AuthenticationException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (User) authentication.getPrincipal();
         Long memberId = Long.valueOf(userDetails.getUsername());
@@ -26,5 +30,14 @@ public class MemberServiceImpl implements MemberService {
         Member currentMember = memberRepository.findById(memberId).orElseThrow(); // TODO: 예외처리
         return currentMember;
     }
+
+    @Override
+    @Transactional
+    public Member updateInfo(String nickname, Gender gender, Integer birthyear) {
+        Member member = getCurrentMember();
+        member.updateInfo(nickname, gender, birthyear);
+        return member;
+    }
+
 
 }
