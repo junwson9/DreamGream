ab26e404bf16ad1ee5087c8367ad06996b4c1474
Jisoo <spritaa@gmail.com>
2023-07-31 02:30:13 +0900

    FEAT: 회원 정보 조회, 회원 정보 수정 기능 추가
    회원 정보 조회, 회원 정보 수정 기능 추가

--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/domain/member/controller/MemberController.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/domain/member/controller/MemberController.java
@@ -1,14 +1,27 @@
 package com.ssafy.dreamgream.domain.member.controller;
 
+import static org.springframework.http.ResponseEntity.badRequest;
+
+import com.ssafy.dreamgream.domain.member.dto.request.UpdateInfoRequestDto;
+import com.ssafy.dreamgream.domain.member.entity.Member;
+import com.ssafy.dreamgream.domain.member.enums.Gender;
 import com.ssafy.dreamgream.domain.member.service.MemberService;
+import java.util.List;
+import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,5 +45,28 @@ public class MemberController {
         log.info(String.valueOf(SecurityContextHolder.getContext()));
         return ResponseEntity.ok(authentication);
     }
+
+    @GetMapping("/info")
+    public ResponseEntity<?> getInfo() {
+        Member member = memberService.getCurrentMember();
+        return ResponseEntity.ok(member);
+    }
+
+    @PutMapping("/info")
+    public ResponseEntity<?> updateInfo(@RequestBody @Validated UpdateInfoRequestDto requestDto, Errors errors) {
+        String nickname = requestDto.getNickname();
+        Gender gender = requestDto.getGender();
+        Integer birthyear = requestDto.getBirthyear();
+        log.info("nickname: {}, gender: {}, birthyear: {}", nickname, gender, birthyear);
+        if(errors.hasErrors()) {
+            // TODO 예외처리 필요
+            return new ResponseEntity<>("회원정보를 변경할 수 없습니다.", HttpStatus.BAD_REQUEST);
+
+        }
+        Member member = memberService.updateInfo(nickname, gender, birthyear);
+
+        return ResponseEntity.ok(member);
+    }
+
 
 }
