00fab4650c92e31031a2ffd9e2e2dd35218b7e61
HBumzz <choi12920@gmail.com>
2023-08-01 00:08:47 +0900

    Feat: 개인피드 기능 구현

--- FrontEnd/dream-gream/src/components/Button/CategoryButtons.jsx
+++ FrontEnd/dream-gream/src/components/Button/CategoryButtons.jsx
@@ -2,31 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function CategoryButtons({ setCategory }) {
-    const categorys = ['여행','건강/운동','배움','문화/엔터','사랑','음식','쇼핑','일','기타']
+  const categorys = [
+    '여행',
+    '건강/운동',
+    '배움',
+    '문화/엔터',
+    '사랑',
+    '음식',
+    '쇼핑',
+    '일',
+    '기타',
+  ];
 
-    const handleClick = (category) => {
-       setCategory(category);
-    };
+  const handleClick = (category) => {
+    setCategory(category);
+  };
 
-    return (
-        <div className="w-[267px] h-[93px] justify-start items-start gap-[9px] inline-flex">
-            {categorys.map((category) => (
-                <button 
-                key={category}
-                className="w-[83px] h-[29px] px-[19px] py-1.5 bg-white rounded-[10px] border border-indigo-400 justify-center items-center gap-2.5 flex"  
-                type='submit' onClick={() =>handleClick(category)}>
-                    <div className="text-center text-indigo-400 text-[13px] font-bold leading-[18.20px]">
-                    {category}
-                    </div>
-                </button>
-            ))}
-            
-        </div>
-    )
+  return (
+    <div className="w-[267px] h-[93px] justify-start items-start gap-[9px] inline-flex">
+      {categorys.map((category) => (
+        <button
+          key={category}
+          className="w-[83px] h-[29px] px-[19px] py-1.5 bg-white rounded-[10px] border border-indigo-400 justify-center items-center gap-2.5 flex"
+          type="submit"
+          onClick={() => handleClick(category)}
+        >
+          <div className="text-center text-indigo-400 text-[13px] font-bold leading-[18.20px]">
+            {category}
+          </div>
+        </button>
+      ))}
+    </div>
+  );
 }
 
 CategoryButtons.propTypes = {
-    setCategory: PropTypes.func.isRequired,
-  };
+  setCategory: PropTypes.func.isRequired,
+};
 
 export default CategoryButtons;
--- FrontEnd/dream-gream/src/pages/MyFeed/MyFeed.jsx
+++ FrontEnd/dream-gream/src/pages/MyFeed/MyFeed.jsx
@@ -1,47 +1,95 @@
 /* eslint-disable */
-import React, { useState } from 'react';
-import CategoryButtons from '../../components/Button/CategoryButtons';
+
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import CategoryButtons from '../../components/Button/CategoryButtons2';
 
 function MyFeed() {
+  const [posts, setPosts_list] = useState([]);
   const [category, setCategory] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const posts = [
-    { id: 1, category: '여행', content: '여행 게시물입니다.' },
-    { id: 2, category: '건강/운동', content: '건강/운동 게시물입니다.' },
-    // 기타 게시물 등
-  ];
+  const member = {
+    followers: 205,
+    followings: 210,
+    achived_score: 70,
+  };
+  // 테스트용
+
+  // const posts = [
+  //   { id: 1, category: '여행', content: '여행 게시물입니다.' },
+  //  { id: 2, category: '건강/운동', content: '건강/운동 게시물입니다.' },
+  //  { id: 3, category: '음식', content: '음식 게시물입니다.' },
+  //  { id: 4, category: '건강/운동', content: '건강/운동 게시물입니다.' },
+  //  { id: 5, category: '쇼핑', content: '쇼핑 게시물입니다.' },
+  //  { id: 6, category: '일', content: '일 게시물입니다.' },
+  // ];
+
   const postsNums = posts.length;
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:8000/api/posts?members={member_id}',
+        );
+        const data = await response.json();
+        console.log(data);
+        // 여기 data가 post_list일지도
+        setPosts(data.post_list);
+      } catch (error) {
+        console.error('Error getting posts: ', error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   const handleCategoryChange = (newCategory) => {
     if (category === newCategory) {
-      setCategory(''); // 동일한 카테고리를 다시 누른 경우 전체 게시글을 보여주려면 카테고리를 초기화합니다.
+      setCategory('');
     } else {
-      setCategory(newCategory); // 새 카테고리가 선택된 경우에만 카테고리를 업데이트합니다.
+      setCategory(newCategory);
     }
+    setIsModalOpen(false);
   };
-
-  const filteredPosts = posts.filter((post) => {
-    if (category === '') {
-      // 카테고리가 선택되지 않은 경우 모든 게시물 반환
-      return true;
-    }
-    return post.category === category;
-  });
 
   return (
     <div>
-      <h1>Page</h1>
-      <h3>{postsNums}</h3>
-      <CategoryButtons setCategory={handleCategoryChange} />
-      <p>선택한 카테고리: {category === '' ? '전체' : category}</p>
+      <h1>nickname</h1>
+      <p>
+        달성률 : {member.achived_score} | 팔로워: {member.followers} | 팔로잉:{' '}
+        {member.followings} |
+      </p>
+      <h3>게시물수 : {postsNums}개</h3>
+      <button onClick={() => setIsModalOpen(true)}>
+        {category === '' ? '전체' : category} ▼
+      </button>
+
+      {/* 모달창 */}
+      <div style={{ display: isModalOpen ? 'block' : 'none' }}>
+        <CategoryButtons setCategory={handleCategoryChange} />
+        <button onClick={() => setIsModalOpen(false)}>닫기</button>
+      </div>
 
       {/* 필터링된 게시물 목록 렌더링 */}
       <ul>
-        {filteredPosts.map((post) => (
-          <li key={post.id}>{post.content}</li>
-        ))}
+        {posts
+          .filter((post) => {
+            if (category === '') {
+              return true;
+            }
+            return post.category.category_name === category;
+          })
+          .map((post) => (
+            <li key={post.post_id}>{post.content}</li>
+          ))}
       </ul>
     </div>
   );
 }
+
+MyFeed.propTypes = {
+  setCategory: PropTypes.func.isRequired,
+};
 
 export default MyFeed;
