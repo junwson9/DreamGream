ae5336be9be79b526ccbfb5388766af69809e09a
Jisoo <spritaa@gmail.com>
2023-07-31 13:02:43 +0900

    FEAT: 공통 ResponseDto 추가
    공통 ResponseDto 양식 추가

---
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/common/dto/response/ResponseDto.java
@@ -0,0 +1,32 @@
+package com.ssafy.dreamgream.global.common.dto.response;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Map;
+
+@Getter
+@ToString
+public class ResponseDto {
+
+    private String status;
+    private String message;
+    private String errorCode;
+    private Map<String, Object> data;
+
+    public ResponseDto() {
+    }
+
+    public ResponseDto(String status, String message, String errorCode, Map<String, Object> data) {
+        this.status = status;
+        this.message = message;
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+
+    public ResponseDto(String status, String message, Map<String, Object> data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+}
