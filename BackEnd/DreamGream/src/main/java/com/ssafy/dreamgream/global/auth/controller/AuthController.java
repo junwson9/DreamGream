80416c608a928aef7371399ac3977bba674eb352
Jisoo <spritaa@gmail.com>
2023-07-31 13:43:02 +0900

    REFACTOR: Response 양식 통일
    Response 양식 통일

--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/auth/controller/AuthController.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/auth/controller/AuthController.java
@@ -4,10 +4,12 @@ import com.ssafy.dreamgream.domain.member.enums.Gender;
 import com.ssafy.dreamgream.global.auth.dto.request.UpdateRoleToUserRequestDto;
 import com.ssafy.dreamgream.global.auth.dto.response.TokenResponseDto;
 import com.ssafy.dreamgream.global.auth.service.AuthService;
+import com.ssafy.dreamgream.global.common.dto.response.ResponseDto;
 import com.ssafy.dreamgream.global.jwt.JwtTokenProvider;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @Slf4j
@@ -33,7 +37,8 @@ public class AuthController {
 		String accessToken = jwtTokenProvider.resolveAccessToken(request);
 		String refreshToken = jwtTokenProvider.resolveRefreshToken(request);
 		TokenResponseDto tokenResponseDto = authService.reissue(accessToken, refreshToken);
-		return ResponseEntity.ok(tokenResponseDto);
+		ResponseDto responseDto = new ResponseDto(success, "토큰을 재발급했습니다.", Collections.singletonMap("token", tokenResponseDto));
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 
 	@PutMapping("/role")
@@ -41,7 +46,8 @@ public class AuthController {
 		Gender gender = requestDto.getGender();
 		Integer birthyear = requestDto.getBirthyear();
 		TokenResponseDto tokenResponseDto = authService.updateRoleToUser(gender, birthyear);
-		return ResponseEntity.ok(tokenResponseDto);
+		ResponseDto responseDto = new ResponseDto(success, "추가 정보 입력을 완료했습니다.", Collections.singletonMap("token", tokenResponseDto));
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 
 	@PostMapping("/logout")
@@ -49,7 +55,7 @@ public class AuthController {
 		String accessToken = jwtTokenProvider.resolveAccessToken(request);
 		log.info("로그아웃 accessToken: {}", accessToken);
 		authService.logout(accessToken);
-		return ResponseEntity.ok(success);
+		return new ResponseEntity<>(new ResponseDto(success, "로그아웃했습니다."), HttpStatus.OK);
 	}
 
 }
