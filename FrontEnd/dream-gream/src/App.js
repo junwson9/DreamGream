c17f81e8a14c1e3d49dc0a166149e4fd778128f4
HBumzz <choi12920@naver.com>
2023-07-31 15:09:36 +0900

    FEAT: 회원 기능 완료(예외처리 제외)

--- FrontEnd/dream-gream/src/App.js
+++ FrontEnd/dream-gream/src/App.js
@@ -4,14 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import PostSubject from './pages/Posting/postSubject';
 import PostViewImage from './pages/Posting/postViewImage';
 import PostDetail from './pages/Posting/postDetail';
+import LogIn from './pages/members/Logintest';
+import OAuth2RedirectHandler from './pages/members/OAuth2RedirectHandler';
+import ProfileEdit from './pages/members/ProfileEdit';
+import SignupGenderBirth from './pages/members/SiguUp';
 
 function App() {
   return (
     <Routes>
-      <Route path="/logIn" element={<logIn />} />
-      <Route path="/oauth2/redirect" element={<oAuth2RedirectHandler />} />
-      <Route path="/Siginupgenderbirth" element={<siginupGenderBirth />} />
-      <Route path="/profileedit" element={<profileEdit />} />
+      <Route path="/logIn" element={<LogIn />} />
+      <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+      <Route path="/Siginupgenderbirth" element={<SignupGenderBirth />} />
+      <Route path="/profileedit" element={<ProfileEdit />} />
       <Route path="/follow" element={<followUser />} />
       <Route path="/following" element={<followingUser />} />
       <Route path="/findmember" element={<findMember />} />
