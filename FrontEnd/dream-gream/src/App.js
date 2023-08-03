e6b091eddfc7a47895871e974a022537347a1a0c
nahye2772 <nahye2772@naver.com>
2023-08-03 22:58:36 +0900

    STYLE: 컴포넌트 구조/기능/스타일 작업 컴포넌트들 구조/기능/스타일 추가`

--- FrontEnd/dream-gream/src/App.js
+++ FrontEnd/dream-gream/src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import PostSubject from './pages/Posting/postSubject';
-import PostViewImage from './pages/Posting/postViewImage';
-import PostDetail from './pages/Posting/postDetail';
 import LogIn from './pages/members/Login';
 import OAuth2RedirectHandler from './pages/members/OAuth2RedirectHandler';
 import ProfileEdit from './pages/members/ProfileEdit';
 import SignupGenderBirth from './pages/members/SiguUp';
 import MyFeed from './pages/MyFeed/MyFeed';
-import InputBox from './components/InputBox/InputBox';
-// import ToggleSwitch from './components/Button/ToggleButton';
-// import TopBar from './components/Common/Topbar';
-import TopTap from './components/Common/TopTab';
-import Navbar from './components/Common/Navbar';
+import Posting from './pages/Posting/Posting';
+import CheerUpFeed from './pages/Feed/cheerUpFeed';
 
 function App() {
   return (
@@ -26,18 +20,16 @@ function App() {
         <Route path="/follow" element={<followUser />} />
         <Route path="/following" element={<followingUser />} />
         <Route path="/findmember" element={<findMember />} />
-        <Route path="/cheerupfeed" element={<cheerUpFeed />} />
+        <Route path="/cheerupfeed" element={<CheerUpFeed />} />
         <Route path="/acheivefeed" element={<acheiveFeed />} />
         <Route path="/feedid" element={<feedDetail />} />
-        <Route path="/post" element={<PostSubject />} />
-        <Route path="/postdetail" element={<PostDetail />} />
-        <Route path="/image" element={<PostViewImage />} />
+        <Route path="/post" element={<Posting />} />
         <Route path="/myfeed" element={<MyFeed />} />
         <Route path="/about" element={<viewAbout />} />
         <Route path="/updatepost" element={<updatePost />} />
         <Route path="/acheivementupdate" element={<acheivementUpdate />} />
       </Routes>
-      </div>
+    </div>
   );
 }
 
--- FrontEnd/dream-gream/src/components/Feed/Member.jsx
+++ FrontEnd/dream-gream/src/components/Feed/Member.jsx
@@ -2,39 +2,73 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { useState } from 'react';
 import { ReactComponent as MoreVert } from '../../assets/icons/MoreVert.svg';
+import ModalForShare from './ModalForShare';
+import ModalForMine from './ModalForMine';
 
 function Member(props) {
+  // 모달창 노출 여부 state
+  const [shareModalOpen, setShareModalOpen] = useState(false);
+  const [mineModalOpen, setMineModalOpen] = useState(false);
+
+  // 모달창 노출
+  const showShareModal = () => {
+    setShareModalOpen(true);
+    setMineModalOpen(false);
+  };
+  const showMineModal = () => {
+    setMineModalOpen(true);
+  };
+
   return (
-    <div className="w-[347px] h-10 pl-[13px] pr-[15px] justify-center items-center gap-[99px] inline-flex">
-      <div className="h-10 justify-start items-center gap-[7px] inline-flex">
-        <img
-          className="w-[39.67px] h-10 rounded-[999px]"
-          src="https://via.placeholder.com/40x40"
-          alt="프로필 이미지"
-        />
-        <div className="w-[200px]">
-          <span className="text-black text-sm font-normal leading-tight">
-            자카타파하
-            {/* ++props에서 넘겨주는건 post 데이터라 닉네임을 post테이블에 추가하던가.. 얘기해봐야함 */}
-            {props.nickname}
-            <br />
-          </span>
-          <span className="text-neutral-600 text-xs font-light leading-none">
-            2023.07.20
-            {props.created_date}
-          </span>
-          <span className="text-indigo-500 text-xs font-medium leading-none">
-            · 1년 이내 {props.deadline}
-          </span>
-          {/* ##자신의 비공개 게시물만 보이도록 하는건 백쪽에서 그렇게 넘겨줄듯? */}
-          {props.is_diplay ? null : (
-            <span className="text-indigo-500 text-xs font-medium leading-none">
-              · 비공개
+    <div className="member-container w-[360px] h-[75px] relative">
+      <div className="w-[347px] h-10 pl-[13px] pr-[15px] left-[6px] top-[18px] absolute justify-center items-center gap-[99px] inline-flex">
+        <div className="h-10 justify-start items-center gap-[7px] inline-flex">
+          <img
+            className="w-[39.67px] h-10 rounded-[999px]"
+            src="https://via.placeholder.com/40x40"
+            alt="프로필 이미지"
+          />
+          <div className="w-[200px]">
+            <span className="text-black text-sm font-normal leading-tight">
+              자카타파하
+              {/* ++props에서 넘겨주는건 post 데이터라 닉네임을 post테이블에 추가하던가.. 얘기해봐야함 */}
+              {props.nickname}
+              <br />
             </span>
-          )}
+            <span className="text-neutral-600 text-xs font-light leading-none">
+              2023.07.20
+              {props.created_date}
+            </span>
+            <span className="text-indigo-500 text-xs font-medium leading-none">
+              · 1년 이내 {props.deadline}
+            </span>
+            {/* ##자신의 비공개 게시물만 보이도록 하는건 백쪽에서 그렇게 넘겨줄듯? */}
+            {props.is_diplay ? null : (
+              <span className="text-indigo-500 text-xs font-medium leading-none">
+                · 비공개
+              </span>
+            )}
+          </div>
         </div>
+        <button
+          type="button"
+          onClick={
+            props.member_id === 'logInUser' ? showShareModal : showMineModal
+          }
+        >
+          <MoreVert />
+        </button>
       </div>
-      <MoreVert />
+      {/* 로그인한 유저 본인게시물이냐에 따라 shareModal을 바로 렌더링하는건 고민해봐야할듯 */}
+      {shareModalOpen && (
+        <ModalForShare setShareModalOpen={setShareModalOpen} />
+      )}
+      {mineModalOpen && (
+        <ModalForMine
+          setMineModalOpen={setMineModalOpen}
+          setShareModalOpen={setShareModalOpen}
+        />
+      )}
     </div>
   );
 }
