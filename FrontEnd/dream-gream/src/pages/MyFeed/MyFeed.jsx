2f44a019b80f0d398bf71cab856f2200a1d8acda
HBumzz <choi12920@naver.com>
2023-08-04 17:54:02 +0900

    FEAT: 마이페이지 구조 작업

--- FrontEnd/dream-gream/src/pages/MyFeed/MyFeed.jsx
+++ FrontEnd/dream-gream/src/pages/MyFeed/MyFeed.jsx
@@ -2,21 +2,14 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
-// import CategoryButtons from '../../components/Button/CategoryButtons2';
 import TopBar from '../../components/Common/Topbar';
+
 function MyFeed() {
-  const [post_list, setPost_list] = useState([]);
+  const [post, setPost] = useState();
+  const [user, setUser] = useState();
   // const [category, setCategory] = useState('');
   // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // const member = {
-  //   followers: 205,
-  //   followings: 210,
-  //   achived_score: 70,
-  // };
-
-  // const postsNums = posts.length;
   const ACCESS_TOKEN = localStorage.getItem('ACCESS_TOKEN');
 
   useEffect(() => {
@@ -32,6 +25,30 @@ function MyFeed() {
           },
         );
         const data = response.data.data;
+        setPost(data);
+        // console.log(data);
+      } catch (error) {
+        console.error('Error while fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          'http://i9a609.p.ssafy.io:8000/api/members/info',
+          {
+            headers: {
+              Authorization: `Bearer ${ACCESS_TOKEN}`,
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+        const data = response.data.data.member;
+        setUser(data);
         console.log(data);
       } catch (error) {
         console.error('Error while fetching data:', error);
@@ -54,7 +71,7 @@ function MyFeed() {
     <div className="w-[360px] h-[800px] relative bg-white">
       <div className="w-[360px] h-[60px] left-0 top-0 absolute">
         <TopBar
-          title="송준우"
+          title={user.nickname}
           showConfirmButton={false}
           showCloseButton={false}
         />
