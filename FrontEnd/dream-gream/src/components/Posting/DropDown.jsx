a41a96cabf394817f98cf389e719492243b603cc
송준우 <songjunwoo@junsong.local>
2023-08-08 00:23:58 +0900

    FIX: git 대소문자 변경 감지 추가

---
+++ FrontEnd/dream-gream/src/components/Posting/DropDown.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable */
+//대문자로 가자
+import React from 'react';
+
+function DropDown({ value, setIsOpen, onChangePeriod, isOpen }) {
+    const handleItemClick = () => {
+      setIsOpen(false);
+      onChangePeriod(value);
+    };
+  
+    return (
+      <li
+        onClick={handleItemClick}
+        style={{
+          margin: '8px 0', //상하 간격을 조정
+          padding: '8px 16px',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          transition: 'background-color 0.2s',
+        }}
+        onMouseEnter={(e) => {
+          e.target.style.backgroundColor = '#f0f0f0';
+        }}
+        onMouseLeave={(e) => {
+          e.target.style.backgroundColor = 'transparent';
+        }}
+      >
+        {value}
+      </li>
+    );
+  }
+    
+export default DropDown;
