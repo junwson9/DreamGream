6a3b8872a216495578461c11e2b0707565b0eabf
HBumzz <choi12920@gmail.com>
2023-08-08 00:22:04 +0900

    FEAT: 친구찾기 기능 구현

--- FrontEnd/dream-gream/src/pages/members/FindMember.jsx
+++ FrontEnd/dream-gream/src/pages/members/FindMember.jsx
@@ -1,29 +1,32 @@
 /* eslint-disable */
-
 import React, { useState } from 'react';
 import TopBar from '../../components/Common/Topbar';
 import InputBox from '../../components/InputBox/InputBox';
-// import { ReactComponent as SearchIcon } from '../../assets/icons/SearchIcon.svg';
+import MemberItem from '../../components/Member/MemberItem';
+
+// isFollowed 처리만 해주고 프로필이나 이름 누르면 navigate되게끔
 
 function FindMember() {
   const [nickname, setNickname] = useState('');
   const [noResult, setNoResult] = useState(false);
-
+  const [memberList, setMemberList] = useState([]); // 빈 배열로 초기화
+  // console.log(memberList);
   const handleSaveClick = (value) => {
     setNickname(value);
-    // console.log('Saved nickname:', value);
-    const queryParams = new URLSearchParams({ nickname: nickname }); // value 체크
-    // console.log(queryParams);
+    const queryParams = new URLSearchParams({ nickname: value }); // 넘어온 값 사용
     const url = `http://i9a609.p.ssafy.io:8000/api/members?${queryParams}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         console.log('Response from server:', data);
-        const memberList = data.data.memberlist;
+        const receivedMemberList = data.data.member_list;
+        setMemberList(receivedMemberList);
 
-        if (memberList === undefined) {
+        // 검색 결과가 없을 때 noResult 상태를 설정
+        if (!receivedMemberList || receivedMemberList.length === 0) {
           setNoResult(true);
-          console.log(1);
+        } else {
+          setNoResult(false);
         }
       })
       .catch((error) => {
@@ -38,21 +41,41 @@ function FindMember() {
         showConfirmButton={false}
         showCloseButton={false}
       />
-      <div class="w-[320px] h-[60px] top-[85px] left-[20px] absolute">
+      <div className="w-[320px] h-[60px] top-[85px] left-[20px] absolute">
         <InputBox
           text={nickname}
           onInputChange={setNickname}
           onSaveClick={handleSaveClick}
         />
       </div>
-      <div
-        className={`${
-          noResult ? 'block' : 'hidden' // hidden 클래스 추가
-        } absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
-      >
-        <div className="font-normal text-center">검색결과가 없습니다.</div>
+      <div>
+        <div className="top-[155px] absolute">
+          {memberList ? (
+            memberList.map((member) => (
+              <div key={member.id}>
+                <MemberItem
+                  nickname={member.nickname}
+                  profileImg={member.profileImg}
+                />
+                {/* 추가적인 정보를 띄우고 싶으면 여기에 추가 */}
+              </div>
+            ))
+          ) : (
+            // memberList가 없거나 길이가 0인 경우 "검색결과가 없습니다." 메시지를 표시합니다.
+            <div
+              className={`${
+                noResult ? 'block' : 'hidden' // hidden 클래스 추가
+              } absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
+            >
+              <div className="font-normal text-center">
+                검색결과가 없습니다.
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 }
+
 export default FindMember;
