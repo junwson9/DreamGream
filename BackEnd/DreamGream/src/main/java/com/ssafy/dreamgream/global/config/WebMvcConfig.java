896e7c3ea39b96267d76ebebe233adfba5671c7d
Jisoo <spritaa@gmail.com>
2023-08-07 17:20:56 +0900

    FIX: Response -> JSON snake case로 자동 변환 기능 수정
    LocalDateTime 직렬화/역직렬화 이슈 해결

--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/config/WebMvcConfig.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/config/WebMvcConfig.java
@@ -2,6 +2,8 @@ package com.ssafy.dreamgream.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-/*	@Bean
+	@Bean
 	public ObjectMapper objectMapper() {
 		ObjectMapper objectMapper = new ObjectMapper();
+		// LocalDateTime 직렬화, 역직렬화에 문제가 있어 코드 추가
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
 		return objectMapper;
-	}*/
+	}
 }
