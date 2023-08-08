f8d8c1ab1dba1d55583a472435fc899b2dc0c85b
Jisoo <spritaa@gmail.com>
2023-08-09 01:09:38 +0900

    FEAT: Auth 예외 처리
    Auth 예외 처리
    
    JWT 토큰 예외처리가 잘 적용되지 않는 이슈 남아있음

--- BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/config/oauth/handler/OAuth2AuthenticationFailureHandler.java
+++ BackEnd/DreamGream/src/main/java/com/ssafy/dreamgream/global/config/oauth/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,5 +1,7 @@
 package com.ssafy.dreamgream.global.config.oauth.handler;
 
+import com.ssafy.dreamgream.global.exception.ErrorCode;
+import com.ssafy.dreamgream.global.exception.customException.OAuth2FailedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,13 +26,13 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException exception) throws IOException, ServletException {
-		// TODO 예외처리
-		log.error("OAuth2 인증 실패:", exception.getMessage());
 
 		String targetUrl = UriComponentsBuilder.fromUriString(redirectUrl)
 			.queryParam("status", "error")
 			.build().toUriString();
 		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+		throw new OAuth2FailedException("OAuth2 인증 실패", ErrorCode.OAUTH2_FAILED);
 	}
 
 }
