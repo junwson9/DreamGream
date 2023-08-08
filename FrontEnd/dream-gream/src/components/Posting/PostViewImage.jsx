66c8621fe458d25f2165ca8e5a203f799588fa62
송준우 <songjunwoo@junsong.local>
2023-08-08 23:30:45 +0900

    FIX: 버킷리스트 생성 기능 구현

--- FrontEnd/dream-gream/src/components/Posting/PostViewImage.jsx
+++ FrontEnd/dream-gream/src/components/Posting/PostViewImage.jsx
@@ -38,11 +38,12 @@ function PostViewImage({ handleCloseIconClick }) {
   console.log(postData);
   const sendPostInfo = async () => {
     try {
+      console.log('게시물 보내!!!!');
       // POST 요청은 body에 실어 보냄
       await axiosInstance.post(`${API_URL}/api/posts`, {
         category_id: postData.subjectData.categoryID,
         title: postData.subjectData.title,
-        content: postData.detailData.detail,
+        content: postData.detailData.content,
         dead_line: postData.detailData.selectedPeriod,
         is_display: postData.detailData.isPublic,
         ai_img: postData.imageUrl.url,
