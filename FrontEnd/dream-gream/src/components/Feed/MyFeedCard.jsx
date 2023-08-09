0042f27bebc7e4bfdbb7fa88e7c7619588bfb766
HBumzz <choi12920@naver.com>
2023-08-09 15:27:34 +0900

    FIX: 마이페이지 렌더링

--- FrontEnd/dream-gream/src/components/Feed/MyFeedCard.jsx
+++ FrontEnd/dream-gream/src/components/Feed/MyFeedCard.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+/* eslint-disable */
 
-function MyFeedCard() {
+import React from 'react';
+import { ReactComponent as CheerUpIcon } from '../../assets/icons/SmallCheerUpIcon.svg';
+
+function MyFeedCard({ title, aiImg, cheerCount }) {
+  const image = {
+    backgroundImage: `url(${aiImg})`, // aiImg를 배경 이미지로 설정
+  };
   return (
     <div className="w-40 h-[221px] flex-col justify-start items-start gap-[3px] inline-flex z-[5] mr-2 mb-2">
-      <div className="w-40 h-40 bg-zinc-300 rounded-[10px] shadow" />
+      <div
+        className="w-40 h-40 bg-zinc-300 rounded-[10px] shadow-md"
+        style={{ ...image, backgroundSize: 'cover' }}
+      />
       <div className="w-[88px] h-[17px] relative">
+        <div className="w-[46px] top-[-0px] absolute text-zinc-800 text-xs font-medium">
+          <CheerUpIcon />
+        </div>
         <div className="w-[46px] left-[18px] top-[-0px] absolute text-zinc-800 text-xs font-medium">
-          999+
+          {cheerCount}
         </div>
       </div>
       <div className="w-40 text-neutral-700 text-[13px] font-normal">
-        주제가 들어가는 곳 주제가 들어가는 곳 주제가 들어...
+        {title}
       </div>
     </div>
   );
