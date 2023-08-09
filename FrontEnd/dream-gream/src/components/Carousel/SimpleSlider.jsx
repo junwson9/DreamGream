c94d6faa39fe2621f48b8ca6246bfdd70299ad54
송준우 <boaz42@khu.ac.kr>
2023-08-09 16:47:26 +0900

    FEAT: 로딩중 랜덤 이미지 추가

--- FrontEnd/dream-gream/src/components/Carousel/SimpleSlider.jsx
+++ FrontEnd/dream-gream/src/components/Carousel/SimpleSlider.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable */
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+
 const SimpleSlider = () => {
+
+  const [randomImageIndexes, setRandomImageIndexes] = useState([]);
+  const baseURL = 'https://dreamgream-image-bucket.s3.ap-northeast-2.amazonaws.com/loading/img_'
   const settings = {
     autoplay: true,
     autoplaySpeed: 3000,
@@ -13,23 +17,29 @@ const SimpleSlider = () => {
     slidesToShow: 1,
     slidesToScroll: 1
   };
+  useEffect(() => {
+    const randomIndexes = [];
+    while (randomIndexes.length < 10) {
+      const randomIndex = Math.floor(Math.random() * 45) + 1;
+      if (!randomIndexes.includes(randomIndex)) {
+        randomIndexes.push(randomIndex);
+      }
+    }
+    setRandomImageIndexes(randomIndexes);
+  }, []);
 
   return (
     <div className="w-48 h-48 rounded-xl">
       <Slider {...settings}>
-        <div className="flex flex-col h-auto">
-          <p className="text-center whitespace-normal text-zinc-800 text-lg font-regular">테스트중입니담ㄴㅇㅎ나엏ㄴ아ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㅎㄴㅇㄻㄴㅇㄹ</p>
-          <img className="w-48 h-48 rounded-xl" src="https://ifh.cc/g/KyG9oQ.png/150x150" alt=''/>
-        </div>
-        <div className="flex flex-col h-auto">
-          <p className="text-center whitespace-normal text-zinc-800 text-lg font-regular">안녕하세요 ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ</p>
-          <img className="w-48 h-48 rounded-xl" src="https://ifh.cc/g/KyG9oQ.png/150x150" alt=''/>
-        </div>
-        <div className="flex flex-col h-auto">
-          <p className="text-center whitespace-normal text-zinc-800 text-lg font-regular">그래앨앨애랭래애랭래ㅓㅁㄴㅇ라ㅣㅓㄴㅁ아렁ㄴ마럼ㄴ아ㅓㄻ나</p>
-          <img className="w-48 h-48 rounded-xl" src="https://ifh.cc/g/KyG9oQ.png/150x150" alt=''/>
-        </div>
-        {/* ... The other slides with similar structure */}
+      {randomImageIndexes.map((index) => (
+          <div key={index} className="flex flex-col h-auto">
+            <img
+              className="w-48 h-48 rounded-xl"
+              src={`${baseURL}${index}.png/150x150`}
+              alt={`Image ${index}`}
+            />
+          </div>
+        ))}
       </Slider>
     </div>
   );
