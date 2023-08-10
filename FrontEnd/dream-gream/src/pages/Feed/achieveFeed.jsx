2f89febd49f17c567bf2d0e87ceee5fe05898266
송준우 <boaz42@khu.ac.kr>
2023-08-10 15:45:21 +0900

    FEAT: 달성 완료 피드 무한스크롤 구현

--- FrontEnd/dream-gream/src/pages/Feed/achieveFeed.jsx
+++ FrontEnd/dream-gream/src/pages/Feed/achieveFeed.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect,Fragment } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import ReactPaginate from 'react-paginate';
+import { useInView } from 'react-intersection-observer';
 import Topbar from '../../components/Common/Topbar';
 import CategoryButtons from '../../components/Button/CategoryButtons';
 import BestBucketList from '../../components/Feed/BestBucketList';
@@ -9,24 +9,24 @@ import FeedForExplore from '../../components/Feed/FeedForExplore';
 import Member from '../../components/Feed/Member';
 import ScrapCelebrateBtns from '../../components/Button/ScrapCelebrateBtns';
 import ToTopButton from '../../components/Button/ToTopButton';
+import UseInfiniteScroll from '../../utils/useInfiniteScroll';
 import { API_URL } from '../../config';
 
 function AchieveFeed() {
-  const [postList, setPostList] = useState([]);
   const [bestBucketList, setBestBucketList] = useState([]);
-
-  const navigate = useNavigate();
-
+  const {ref,inView} = useInView()
+  const fetchInfiniteScrollData = (pageParam) => 
+     UseInfiniteScroll(pageParam, 10) // Use your custom hook here
+  ;
+  const { data: postInfoList, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ['infinitePostList'],
+    ({pageParam = 999999 }) => fetchInfiniteScrollData(pageParam),
+    {
+      getNextPageParam: (lastPage) => !lastPage.isLast ? lastPage.nextLastPostId : undefined
+    }
+  );
+  
   useEffect(() => {
-    axios
-      .get(`${API_URL}/api/posts/achieved`)
-      .then((response) => {
-        setPostList(response.data.data.post_list.content);
-        console.log(response);
-        console.log('달성후 전체 게시글 조회에 성공');
-      })
-      .catch((error) => console.log(error));
-
     axios
       .get(`${API_URL}/api/posts/best/achieved`)
       .then((response) => {
@@ -36,6 +36,11 @@ function AchieveFeed() {
       })
       .catch((error) => console.log(error));
   }, []);
+  useEffect(() => {
+    if (inView) 
+    {fetchNextPage();
+  }
+  }, [inView]);
 
   return (
     <div className="body" style={{ overflow: 'auto', overflowX: 'hidden' }}>
@@ -54,16 +59,19 @@ function AchieveFeed() {
       <br />
       <hr />
       <div className="main">
-        {postList.map((post) => (
-          <div className="article" key={post.postId}>
-            <Member post={post} />
-            <FeedForExplore post={post} />
-            <ScrapCelebrateBtns post={post} />
+      {postInfoList?.pages.map((page) => (
+          <Fragment key={page.postId}>
+            <div className="article" key={page.postId}>
+              <Member post={page} />
+              <FeedForExplore post={page} />
+              <ScrapCelebrateBtns post={page} />
             <br />
             <br />
             <hr />
           </div>
+          </Fragment>
         ))}
+        {isFetchingNextPage ? <div>로딩중</div> : <div ref={ref} />}
       </div>
       <div className="w-[360px] h-[66px] pl-[79px] pr-[81px] pt-[21px] pb-[11px] bg-white bg-opacity-0 flex-col justify-end items-center gap-0.5 inline-flex">
         <div className="text-center text-neutral-400 text-[11px] font-normal">
