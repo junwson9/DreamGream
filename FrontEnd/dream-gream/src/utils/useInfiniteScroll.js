4df53e3d592e1ba1ec6b245ba696870310f235b1
송준우 <boaz42@khu.ac.kr>
2023-08-10 17:54:18 +0900

    FEAT: 무한스크롤 기능 구현중

--- FrontEnd/dream-gream/src/utils/useInfiniteScroll.js
+++ FrontEnd/dream-gream/src/utils/useInfiniteScroll.js
@@ -2,10 +2,14 @@
 import { API_URL } from '../config';
 import axiosInstance from './axiosInterceptor';
 
-export default UseInfiniteScroll = async (lastPostID, size) => {
+const UseInfiniteScroll = async (lastPostID, size) => {
     const res = await axiosInstance.get(
-    `${API_URL}/api/posts/achieved?last_post_id=${lastPostID}`
+    `${API_URL}/api/posts/achieved?last-post-id=${lastPostID}`,
     );
     const postList = res.data.data.post_list.content
-  return { postList,nextLastPostId: postList[postList.length - 1]?.postId, isLast: postList.length < size };
+    console.log(postList);
+    console.log(postList[postList.length - 1]?.post_id)
+  return { postList, nextLastPostId: postList[postList.length - 1]?.post_id, isLast: postList.length < size };
 };
+
+export default UseInfiniteScroll;
