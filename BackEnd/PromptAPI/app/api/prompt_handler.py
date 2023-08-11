10891907b4260600ca99348cedade15e7708b8b4
kimjunhyun <idkim8671@gmail.com>
2023-08-11 17:26:22 +0900

    REFACTOR: Prompt 최적화 Prompt 최적화

--- BackEnd/PromptAPI/app/api/prompt_handler.py
+++ BackEnd/PromptAPI/app/api/prompt_handler.py
@@ -17,7 +17,7 @@ class PromptHandler:
                                        engine='text-davinci-003', # gpt 엔진 종류
                                        prompt=prompt, # prompt 변수 전달
                                        max_tokens=70, # 응답 길이 설정
-                                       temperature=0.7, # 낮을수록 일관성 상승 / 높을수록 다양성 상승
+                                       temperature=0.5, # 낮을수록 일관성 상승 / 높을수록 다양성 상승
                                        n=1, # 답변 받을 개수
                                        stop=None, # 일정 토큰량 넘어가면 함수 호출 스탑
                                        timeout=10 # 응답 최대 대기 시간
@@ -25,9 +25,6 @@ class PromptHandler:
         # 응답에서 필요한 부분만 추출
         if 'choices' in response and len(response['choices']) > 0:
             choices = response['choices']
-            #print(type(choices))
-            #print(choices)
-            #print(choices[0]['text'].strip())
             return {'image_prompt': choices[0]['text'].strip()}
         return None
 
@@ -51,7 +48,8 @@ class PromptHandler:
         category = ["남성 1명과 여성 1명이 사이 좋게 있는 모습 또는 내가 알려준 버킷리스트 주인의 성별과는 다른 성별의 그림을 만들 수 있도록 명령어를 알려줘.",
                     "만약 title에 취업하기,취뽀,취직하기 라는 단어가 들어가면 회사와 관련된 그림을 그릴 수 있게 문장을 작성해",
                     "please write a new sentence that related title and includes the word 'dollar.' ",
-                    "부자에 관련된 문장을 작성하면 돼."]
+                    "부자에 관련된 문장을 작성하면 돼.",
+                    ""]
     
         # LangChain PromptTemplate 사용
         prompt = PromptTemplate(
