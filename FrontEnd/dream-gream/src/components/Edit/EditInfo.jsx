5567cf419e168bfb7352ac851fbc774d249620ec
송준우 <songjunwoo@junsong.local>
2023-08-20 14:12:35 +0900

    STYLE: 공개범위/목표시기 간격 조정

--- FrontEnd/dream-gream/src/components/Edit/EditInfo.jsx
+++ FrontEnd/dream-gream/src/components/Edit/EditInfo.jsx
@@ -17,10 +17,10 @@ function EditInfo({
   return (
     <div>
       <div className="w-[360px] h-14 mt-[18px] relative bg-white">
-        <div className="left-[22px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
+        <div className="left-[30px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
           카테고리
         </div>
-        <div className="left-[270px] top-[17px] mr-[333px] absolute text-center text-zinc-800 text-base font-normal">
+        <div className="right-[30px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
           <ContainerForCategory
             selectedCategory={selectedCategory}
             setSelectedCategory={setSelectedCategory}
@@ -30,10 +30,10 @@ function EditInfo({
         <div className="w-[360px] h-px left-0 top-[1px] absolute border border-neutral-100" />
       </div>
       <div className="w-[360px] h-14 relative bg-white">
-        <div className="left-[22px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
+        <div className="left-[30px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
           목표 시기
         </div>
-        <div className="left-[270px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
+        <div className="right-[30px] top-[17px] absolute text-center text-zinc-800 text-base font-normal">
           <Container
             selectedPeriod={selectedPeriod}
             onChangePeriod={setSelectedPeriod}
@@ -43,10 +43,10 @@ function EditInfo({
         <div className="w-[360px] h-px left-0 top-[1px] absolute border border-neutral-100" />
       </div>
       <div className="w-[360px] h-14 relative bg-white">
-        <div className="left-[22px] top-[18px] absolute text-center text-zinc-800 text-base font-normal">
+        <div className="left-[30px] top-[18px] absolute text-center text-zinc-800 text-base font-normal">
           공개 설정
         </div>
-        <div className="w-12 h-6 left-[294px] top-[16px] absolute">
+        <div className="w-12 h-6 right-[30px] top-[16px] absolute">
           <ToggleButton isChecked={isPublic} onToggle={onTogglePublic} />
         </div>
         <div className="w-[360px] h-px left-0 top-[1px] absolute border border-neutral-100" />
